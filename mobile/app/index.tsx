a0f026893ee6ba5d23e16851e2517cd74e2a1aa1
rahulkardile <rahulkardile321@gmail.com>
2025-07-13 08:55:22 +0530

    basics of react native

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,15 +1,52 @@
-import { Text, View } from "react-native";
+import { Image } from "expo-image";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{
+        color: "red"
+      }}>Making sure everything works well</Text>
+
+      <Link href={"/about"}> About </Link>
+
+      {/* <Image 
+        source={{
+          uri: "https://avatars.githubusercontent.com/u/122984037?v=4"
+        }}
+
+        style={{
+          width: 100, 
+          height: 100
+        }}
+      /> */}
+
+      <Image
+        source={require("@/assets/images/react-logo.png")}
+        style={{
+          width: 100,
+          height: 100
+        }}
+      />
+
+      <View>
+
+        <Text> Hello Native </Text>
+
+      </View>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "gray",
+  }
+})
