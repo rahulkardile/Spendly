7b435566b34f3e9c89efe70160fcd2a741b45a1f
rahulkardile <rahulkardile321@gmail.com>
2025-07-17 23:03:23 +0530

    basic authentication setup

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,8 +1,15 @@
 import SafeScreen from "@/components/SafeScreen";
-import { Stack } from "expo-router";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { Slot, Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <SafeScreen>
-    <Stack screenOptions={{ headerShown: false }} />
-  </SafeScreen>;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeScreen>
+        <Slot />
+        {/* <Stack screenOptions={{ headerShown: false }} /> */}
+      </SafeScreen>;
+    </ClerkProvider>
+  )
 }
